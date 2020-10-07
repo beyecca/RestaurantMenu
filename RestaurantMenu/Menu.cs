24b6e9e3125a957d9032b4662cc087e3f71cb5be
Becca Henegar <rh4xfh@gmail.com>
2020-10-06 21:40:54 -0500

    Add project files.

---
+++ RestaurantMenu/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RestaurantMenu
+{
+    public class Menu
+    {
+        public DateTime LastUpdated { get; private set; }
+        public List<MenuItem> MenuItems { get; private set; }
+
+        public Menu()
+        {
+            LastUpdated = DateTime.Now;
+            MenuItems = new List<MenuItem> 
+            { 
+                // The 'M' here forces 10.00 to be a 'decimal' type, 
+                // rather than a 'double', which is standard for dollar representations in C#
+                new MenuItem(10.00M, "pizza", Category.MainCourse, false),
+                new MenuItem(8.00M, "cheesecake", Category.Dessert, true),
+                new MenuItem(4.00M, "fries", Category.Appetizer, false)
+            };
+        }
+    }
+}
---
+++ RestaurantMenu/MenuItem.cs
@@ -0,0 +1,34 @@
+﻿namespace RestaurantMenu
+{
+    public class MenuItem
+    {
+        // I used {get; private set;} syntax here because it is shorter 
+        // and allows me to access each property on a new instance of the class, like:
+        //      MenuItem item = new MenuItem()
+        //      decimal price = item.Price
+        //
+        // but protects me from setting the property:
+        //      MenuItem item = new MenuItem()
+        //      item.Price = 12.00M (this will throw an error)
+
+        public decimal Price { get; private set; }
+        public string Description { get; private set; }
+        public Category Category { get; private set; }
+        public bool IsNew { get; private set; }
+
+        public MenuItem(decimal price, string description, Category category, bool isNew)
+        {
+            Price = price;
+            Description = description;
+            Category = category;
+            IsNew = isNew;
+        }
+    }
+
+    public enum Category 
+    { 
+        Appetizer,
+        MainCourse,
+        Dessert
+    }
+}
